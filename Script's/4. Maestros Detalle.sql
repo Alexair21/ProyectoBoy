54390bc4c94cdd727f2108209d215f1678c2bac3
Jonathan Smith Mostacero Camacho <70053591+jsmithmostacero@users.noreply.github.com>
2023-01-15 17:30:19 -0500

    Update. Maestro detalle-Jonathan
    Sentencias de maestro detalle Prestamo-DetallePrestamo, dónde la tabla Prestamo es el maestro y DetallePrestamo el detalle.

--- Script's/4. Maestros Detalle.sql
+++ Script's/4. Maestros Detalle.sql
@@ -13,3 +13,22 @@ SELECT
     INNER JOIN FICHAS_INSCRIPCION FI on U.FIN_Id = FI.FIN_Id
     INNER JOIN TIPO_RESERVA TR on R.TPR_Id = TR.TPR_Id
 
+
+--------------MAESTRO DETALLE JONATHAN-----------------
+--MAESTRO
+Select P.PRS_Id			AS Codigo,
+		FI.FIN_Nombre	AS	[Nombre Completo],
+		P.PRS_FechaPrestamo AS [Fecha Prestamo],
+		P.PRS_FechaDevolucion AS [Fecha Devolución],
+		TP.TPR_Nombre	AS [Tipo Prestamo]
+	from PRESTAMOS P
+	INNER JOIN USUARIOS U				ON P.USR_Id = U.USR_Id
+	INNER JOIN FICHAS_INSCRIPCION FI	ON U.FIN_Id = FI.FIN_Id
+	INNER JOIN TIPOS_PRESTAMO TP		ON P.TPR_Id = TP.TPR_Id
+
+--DETALLE
+Select DP.PRS_Id	AS Codigo, L.LBR_Titulo	AS Libro, DP.Cantidad AS Cantidad , DP.CantidadHrs AS [N° Hrs]
+	from DETALLE_PRESTAMO DP
+	INNER JOIN PRESTAMOS P ON DP.PRS_Id = P.PRS_Id
+	INNER JOIN LIBROS L ON DP.LBR_Id = DP.LBR_Id
+	group by DP.PRS_Id, L.LBR_Titulo, DP.Cantidad ,DP.CantidadHrs
